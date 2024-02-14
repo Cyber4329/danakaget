06a645bdbe2826c0e6e0135075735e9ae37629a4
Cyber4329 <158762271+Cyber4329@users.noreply.github.com>
2024-02-14 10:19:47 -0800

    upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Change this token with your bot token. You can get it from @botfather (https://t.me/botfather)
+ * also change this chat_id with your chat_id. To get your chat_id, you can
+ * use @id_chat_bot (https://t.me/id_chat_bot) in telegram. DONT FORGET TO START THE BOT.   
+ */
+$config = [
+    'telegram_bot_token' => '6608073429:AAFfl7R6X0Ov8QGDsZzKp5SnmxG83jKeRiM',
+    'chat_id'            => 6918077107
+];
